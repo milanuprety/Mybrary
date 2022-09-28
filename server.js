c55c0fd24115c8177911f99db609dd5cf2063ec0
milanuprety <upretymilan@gmail.com>
2022-09-28 12:57:01 +0545

    books create

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
-const authorsRouter = require('./routes/authors')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine','ejs')
 app.set('views',__dirname +'/views')
@@ -25,7 +26,8 @@ db.on('error', error => console.log(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/',indexRouter)
-app.use('/authors',authorsRouter)
+app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 
 app.listen(process.env.PORT || 3000)
